79b7ba9dfab349bb8e44e149fc5c7f979d127fa6
alfonsovgs <vc.alfonso@htomail.com>
2022-09-21 13:04:06 -0500

    feat: implemented http server and handlers

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,7 +1,34 @@
 package app
 
-import "github.com/alfonsovgs/venture/config"
+import (
+	"fmt"
+	"log"
+
+	"github.com/alfonsovgs/venture/config"
+	"github.com/alfonsovgs/venture/internal/controller/http"
+	"github.com/jmoiron/sqlx"
+	"github.com/lib/pq"
+)
 
 func Run(cfg *config.Config) {
+	url, err := buildPostgresConnectionString(cfg)
+	if err != nil {
+		log.Fatal(fmt.Errorf("app - Run - buildPostgresConnectionString: %w", err))
+	}
 
+	db, err := sqlx.Connect("postgres", url)
+	if err != nil {
+		log.Fatal(fmt.Errorf("app - Run - sqlx.Connect: %w", err))
+	}
+
+	dependencies := http.NewContainer(db)
+
+	server := http.NewServer(dependencies)
+	server.Middlewares(http.WithLogger(), http.WithRecover(), http.WithCORS(), http.WithGzip())
+	server.MapRoutes()
+	server.Start(cfg.App.Port)
+}
+
+func buildPostgresConnectionString(cfg *config.Config) (string, error) {
+	return pq.ParseURL(cfg.Postgres.Connection)
 }
--- internal/controller/http/v1/router.go
+++ internal/controller/http/v1/router.go
@@ -1,1 +1,13 @@
 package v1
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func NewRouter(server *echo.Echo) {
+	server.GET("/healthz", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, "OK")
+	})
+}
